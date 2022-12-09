8506fe915f1994f143532165afbbb4c18351d499
fadimoayad <fadimoayad99@yahoo.com>
2022-12-09 10:31:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for(let i=0;i<word.length;i++){
+    if(word[0] === word[word.length-1]){
+      return true
+    }else{
+  return false
+
+    }
+  }
 }
 
 /* 
   Add your pseudocode here
+  1-create a function called isPalindrome 
+  2 assign a argument to isPalindrome
+  3- create a counter to walk into each letter in the word 
+  4-compare first letter with the last letter 
 */
 
 /*
+isPalindrome function is function to return true (by comparing the first letter with the last letter with if statment) 
+if the first letter was similer to the last letter other wise it return false
   Add written explanation of your solution here
 */
 
